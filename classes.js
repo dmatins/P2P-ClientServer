a8218076ef69008dfca808ecad5caf83c3229e13
mittens <matinsdavid@gmail..com>
2016-11-14 09:45:24 -0700

    added some sweet links

--- classes.js
+++ classes.js
@@ -3,18 +3,22 @@ function drawText(context, x, y, size, color, text){
     context.fillStyle = color;
     context.fillText(text, x, y);
 }
-
-function drawFileBar(context, x, y, packets){
+// fileInfo is an array of objects that contain {color, proportion}
+function drawFileBar(context, x, y, fileInfo){
     var currentY = y;
-    var width;
-    for (i=0; i < packets.length; i++) {
-        context.fillStyle = packets[i].color;
-        context.fillRect(x, currentY, packets[i].width, packets[i].height);
-        currentY += packets[i].height;
-        width = packets[i].width;
+    var width = 5;
+    var barSize = 50;
+    var info;
+    for (var i=0; i < fileInfo.length; i++) {
+        info = fileInfo[i];
+        var amountToFill = info.proportion * barSize;
+
+        context.fillStyle = info.color;
+        context.fillRect(x, currentY, width, amountToFill);
+        currentY += amountToFill;
     }
     context.fillStyle = "grey";
-    context.strokeRect(x, y, width, currentY - y);
+    context.strokeRect(x, y, width, barSize);
 }
 
 function MyServer(x, y, type) {
@@ -24,10 +28,8 @@ function MyServer(x, y, type) {
     this.x = x;
     this.y = y;
     this.color = "orange";
-    this.capturedPackets = [];
+    this.capturedPackets = [{color : this.color, proportion : 1.0}];
     this.draw = function(context) {
-        //context.fillStyle = this.color;
-        //context.fillRect(this.x, this.y, this.width, this.height);
         var imageObj = new Image();
         imageObj.onload = function() {
         };
@@ -38,18 +40,8 @@ function MyServer(x, y, type) {
 
         drawText(context, this.x, this.y - 4, 13, "black", this.type);
     }
-    this.sendPacket = function(destX, destY) {
-        return new MyPacket(this.color, this.x, this.y, destX, destY, this.type);
-    }
-    this.detectCollision = function(obj) {
-        if(obj.type === this.type)
-            return false;
-        if((this.x <= obj.x && obj.x <= this.x + this.width) || (this.x >= obj.x && this.x <= obj.x + obj.width)){
-            if((this.y <= obj.y && obj.y <= this.y + this.height) || (this.y >= obj.y && this.y <= obj.y + obj.height)){
-                this.capturedPackets.push(obj);
-                return true;
-            }
-        }
+    this.sendPacket = function(destX, destY, dest) {
+        return new MyPacket(this.color, this.x, this.y, destX, destY, this.type, dest);
     }
 }
 
@@ -61,10 +53,7 @@ function MyClient(color, x, y, type) {
     this.x = x;
     this.y = y;
     this.capturedPackets = [];
-    this.draw = function(context) {      
-        //context.fillStyle = this.color;
-        //context.fillRect(this.x, this.y, this.width, this.height);
-
+    this.draw = function(context) {
         var imageObj = new Image();
         imageObj.onload = function() {
         };
@@ -75,23 +64,79 @@ function MyClient(color, x, y, type) {
 
         drawText(context, this.x, this.y - 4, 13, "black", this.type);
     }
-    this.sendPacket = function(destX, destY) {
-        return new MyPacket(this.color, this.x, this.y, destX, destY, this.type);
+    this.sendPacket = function(destX, destY, dest) {
+        return new MyPacket(this.color, this.x, this.y, destX, destY, this.type, dest);
     }
     this.detectCollision = function(obj) {
-        if(obj.type === this.type)
+        if(obj == null || obj.from === this.type)
             return false;
         if((this.x <= obj.x && obj.x <= this.x + this.width) || (this.x >= obj.x && this.x <= obj.x + obj.width)){
             if((this.y <= obj.y && obj.y <= this.y + this.height) || (this.y >= obj.y && this.y <= obj.y + obj.height)){
-                this.capturedPackets.push(obj);
+                var newPacket = true;
+                var info;
+                for (var i=0; i < this.capturedPackets.length; i++) {
+                    info = this.capturedPackets[i];
+                    if(info.color === obj.color){
+                        info.proportion += 0.01;
+                        newPacket = false;
+                    }
+                }
+                if(newPacket){
+                    info = {};
+                    info.color = obj.color;
+                    info.proportion = 0.01;
+                    this.capturedPackets.push(info);
+                }
                 return true;
             }
         }
     }
 }
 
-function MyPacket(color, x, y, destX, destY, type) {
-    this.type = type;
+function MyRouterNet(x, y, links) {
+    this.type = "router";
+    this.width = 60;
+    this.height = 50;
+    this.x = x;
+    this.y = y;
+    this.packetBuffer = [];
+    this.links = links;
+    this.draw = function(context) {
+        for (var l in this.links) {
+            var link = links[l];
+            context.beginPath();
+            context.moveTo(link[0].x, link[0].y);
+            context.lineTo(link[1].x, link[1].y);
+            context.strokeStyle = "black";
+            context.lineWidth = 1;
+            context.stroke();
+        }
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+        };
+        imageObj.src = './images/router.png';
+        context.drawImage(imageObj, this.x, this.y, this.width, this.height);
+
+        drawText(context, this.x - 5, this.y - 4, 13, "black", "Router Network");
+    }
+    this.route = function(obj) {
+        if(obj == null || obj.from === this.type)
+            return false;
+        if((this.x <= obj.x && obj.x <= this.x + this.width) || (this.x >= obj.x && this.x <= obj.x + obj.width)){
+            if((this.y <= obj.y && obj.y <= this.y + this.height) || (this.y >= obj.y && this.y <= obj.y + obj.height)){
+                // this should be links instead of x,y
+                var link = this.links[obj.dest];
+                this.packetBuffer.push(new MyPacket(obj.color, link[0].x, link[0].y, link[1].x, link[1].y, this.type, obj.dest));
+                return true;
+            }
+        }
+    }
+}
+
+function MyPacket(color, x, y, destX, destY, from, dest) {
+    this.dest = dest;
+    this.from = from;
     this.color = color;
     this.width = 5;
     this.height = 5;
@@ -115,8 +160,5 @@ function MyPacket(color, x, y, destX, destY, type) {
             else
                 this.y = this.y - 1;
         }
-    }
-    this.detectCollision = function(obj) {
-        return false;
     }
 }
